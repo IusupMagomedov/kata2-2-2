9e5e0db63040c667af0eaaee1b35ab715a4cb59b
iUsup <yusub1988@gmail.com>
2024-12-10 18:01:11 +0300

    fix: Contoller method's name

--- src/main/java/web/controllers/CarsController.java
+++ src/main/java/web/controllers/CarsController.java
@@ -5,18 +5,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.models.Car;
 import web.services.CarService;
 import web.services.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
 public class CarsController {
     @GetMapping()
-    public String index(
+    public String getCars(
             @RequestParam(
                     value = "count",
                     required = false)
