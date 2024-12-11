ee87455f47494321dfb8ae3b67a9177f64f1fcd7
iUsup <yusub1988@gmail.com>
2024-12-11 16:08:19 +0300

    fix: Got rid off get()

--- src/main/java/web/services/CarService.java
+++ src/main/java/web/services/CarService.java
@@ -1,12 +1,8 @@
 package web.services;
 
 import web.models.Car;
-
 import java.util.List;
 
 public interface CarService {
-
     List<Car> get(Integer count);
-
-    List<Car> get();
 }
--- src/main/java/web/services/CarServiceImpl.java
+++ src/main/java/web/services/CarServiceImpl.java
@@ -18,13 +18,8 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public List<Car> get(Integer count) {
-        List<Car> cars = get();
+        List<Car> cars = carDao.findAll();
         int showCount = count != null ? count : 5;
         return cars.stream().limit(showCount).toList();
-    }
-
-    @Override
-    public List<Car> get() {
-        return carDao.findAll();
     }
 }
